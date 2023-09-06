d8aede0663e5a150b4926b0c469681f773a09baa
Jeremy <jeremygprawira@gmail.com>
2023-09-07 02:45:49 +0700

    feat (api): create config and user registration mechanism

---
+++ index.js
@@ -0,0 +1,56 @@
+import express from "express";
+import bodyParser from "body-parser"; // process request body
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer, { diskStorage } from "multer"; // for file upload (part of gridfs)
+import helmet from "helmet"; // request safety
+import morgan from "morgan"; // logging
+import path from "path";
+import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
+
+/* Configurations */
+const __filename = fileURLToPath(import.meta.url); // eslint-disable-line no-underscore-dangle, import/ - grab the file url (specifically when you use the modules)
+const __dirname = path.dirname(__filename);
+dotenv.config(); // so we can use .env files
+const app = express(); // we can use our middleware later on
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets"))); // set the directory where we save our assets
+
+/* File Storage */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+/* Routes with file */
+app.post("/auth/register", upload.single("picture"), register);
+
+/* Routes */
+app.use("/auth", authRoutes);
+
+/* Mongoose */
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
