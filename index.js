1954d061da1f974a0cd04a2238e81a343c712fd5
Jeremy <jeremygprawira@gmail.com>
2023-09-09 18:00:03 +0700

    feat(api): create apis

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ import morgan from "morgan"; // logging
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /* Configurations */
 const __filename = fileURLToPath(import.meta.url); // eslint-disable-line no-underscore-dangle, import/ - grab the file url (specifically when you use the modules)
@@ -39,9 +43,12 @@ const upload = multer({ storage });
 
 /* Routes with file */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* Routes */
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* Mongoose */
 const PORT = process.env.PORT || 6001;
